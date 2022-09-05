91df40dc804cdc4de1c4af7a9fdfd9407b2e6723
Hervé Chigros <herve.chigros@odeven.fr>
2022-09-05 13:00:16 +0200

    contact form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,22 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    modal.classList.add("display");
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+    modal.classList.remove("display");
 }
+
+var form = document.querySelector('#contact_modal form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    var prenom = document.getElementById('prenom').value
+    var nom = document.getElementById('nom').value
+    var email = document.getElementById('email').value
+    var message = document.getElementById('message').value
+    console.log("Prénom : ", prenom, "| Nom : ", nom, "| Mail : ", email, "| Message : ", message)
+    form.reset()
+    closeModal()
+});
